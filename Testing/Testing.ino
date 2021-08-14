947f4bccda8fe252b8e114b39082033cf6bbf3f6
max <mw1919@ic.ac.uk>
2021-08-14 17:16:52 +0100

    pins correctly configured

--- Testing/Testing.ino
+++ Testing/Testing.ino
@@ -1,18 +1,42 @@
+
 // all sensors and valves are numbered from top down
 // float switches
-const int floatSwitch[4]; // float switch pins, index 0 is the top, active LOW 0 means the float is up
+const int floatSwitch[4] = {27,26,25,33}; // float switch pins, index 0 is the top, active LOW 0 means the float is up
 // valves
-const int valve[3]; // valve pins, index 0 is the top
-bool valveOut[3];   // the values to write to the valves at the end of the loop
+const int valve[3] = {19,21,18}; // valve pins, index 0 is the top
+bool valveOut[3] = {false, false, false};   // the values to write to the valves at the end of the loop
 
-const int pump;   // the pump pin out
-bool pumpOut;     // the value to send to the pump at the end of the loop
-const int lights; // the lights pin out
-bool lightsOut;   // the value to send to the lights at the end of the loop
+const int pump = 4;   // the pump pin out
+bool pumpOut = false;     // the value to send to the pump at the end of the loop
+const int lights = 2; // the lights pin out
+bool lightsOut = false;   // the value to send to the lights at the end of the loop
+
+struct WaterState
+{
+    char prevFloatSwitchState[4];              // state of the float switches at the begining of a switch
+    unsigned long prevWaterTime = 0;           // time when the last watering occured
+    unsigned long waterDelay = 10 * 60 * 1000; // time between watering
+};
+
+struct LightsState
+{
+    bool lightState = HIGH;
+    unsigned long doubleprevLightTime = 0;
+    unsigned long lightDelay = 12 * 60 * 60 * 1000;
+};
+
+void water(WaterState &ws);
+void light(LightsState &ls);
+void output();
 
 WaterState waterState;
+LightsState lightsState;
 void setup()
 {
+    //setup
+    Serial.begin(9600);
+    Serial.println("Hello");
+    // set pin modes
     pinMode(floatSwitch[0], INPUT);
     pinMode(floatSwitch[1], INPUT);
     pinMode(floatSwitch[2], INPUT);
@@ -24,6 +48,18 @@ void setup()
 
     pinMode(pump, OUTPUT);
     pinMode(lights, OUTPUT);
+
+    pinMode(21,OUTPUT);
+}
+
+void test(){
+    for (int i = 0; i < 3; i++)
+    {
+        valveOut[i] = LOW;
+    }
+    valveOut[2] = LOW;
+    pumpOut = LOW;
+    lightsOut = HIGH;
 }
 
 void loop()
@@ -31,15 +67,9 @@ void loop()
     water(waterState);
     light(lightsState);
     output();
+    delay(500);
 }
 
-struct WaterState
-{
-    char prevFloatSwitchState[4];              // state of the float switches at the begining of a switch
-    unsigned long prevWaterTime = 0;           // time when the last watering occured
-    unsigned long waterDelay = 10 * 60 * 1000; // time between watering
-};
-WaterState waterState;
 void water(WaterState &ws)
 {
     // initilise all the outputs to LOW
@@ -80,13 +110,6 @@ void water(WaterState &ws)
     pumpOut == !ws.prevFloatSwitchState[3] && digitalRead(floatSwitch[0]);
 }
 
-struct LightsState
-{
-    bool lightState = HIGH;
-    unsigned long doubleprevLightTime = 0;
-    unsigned long lightDelay = 12 * 60 * 60 * 1000;
-};
-LightsState lightsState;
 void light(LightsState &ls)
 {
     lightsOut = ls.lightState;
@@ -100,10 +123,23 @@ void light(LightsState &ls)
 
 void output()
 {
+    //if(digitalRead(floatSwitch[3])){
+    //    pumpOut = LOW;
+    //}
     for (int i = 0; i < 3; i++)
     {
-        digitalWrite(valve[i], valveOut[i]);
+        digitalWrite(valve[i], !valveOut[i]);
     }
-    digitalWrite(pump, pumpOut);
-    digitalWrite(lights, lightsOut);
+    digitalWrite(pump, !pumpOut);
+    lightsOut = (!pumpOut)&&lightsOut; // turn off lights if pump is on
+    digitalWrite(lights, !lightsOut);
+
+    /*Serial.print("Switch0 ");
+    Serial.println(digitalRead(floatSwitch[0]));
+    Serial.print("Switch1 ");
+    Serial.println(digitalRead(floatSwitch[1]));
+    Serial.print("Switch2 ");
+    Serial.println(digitalRead(floatSwitch[2]));
+    Serial.print("Switch3 ");
+    Serial.println(digitalRead(floatSwitch[3]));*/
 }
