781ba43ca108314de51d4c721b23a36e96ca0b8d
max-wickham <60003298+max-wickham@users.noreply.github.com>
2021-10-17 12:37:16 +0100

    test

--- build/sketch/main.ino.cpp
+++ build/sketch/main.ino.cpp
@@ -1,16 +1,23 @@
 #include <Arduino.h>
 #line 1 "/home/max/Sync/Projects/HardwareProjects/LivingLarder (2)/LivingLarder2/src/main/main.ino"
+#include "../Time/Time.h"
 #include "../Hardware/Hardware.h"
+#include "../Communication/Communication.h"
 
 Hardware hardware;
+Communication communication;
+Time time;
+int memoryOffset = 0;
 
 void start(){
-    // TODO add check to see if this is the first run
-    hardware = Hardware();
-    
+    hardware = Hardware(memoryOffset);
+    communication = Communication();
+    time = Time();
 }
 
 void loop(){
-    long int seconds;
+    // seconds since the start of the day
+    unsigned int seconds = time.getSeconds();
     hardware.run(seconds);
+    communication.run(hardware, memoryOffset); // communication will update harware settings
 }
